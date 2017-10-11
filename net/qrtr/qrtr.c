5eca4cc6c0c58282af5df6eaee7923610b9a9cdb
Bjorn Andersson <bjorn.andersson@linaro.org>
2017-10-10 23:45:23 -0700

    net: qrtr: Support decoding incoming v2 packets
    Add the necessary logic for decoding incoming messages of version 2 as
    well. Also make sure there's room for the bigger of version 1 and 2
    headers in the code allocating skbs for outgoing messages.
    
    Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/qrtr/qrtr.c
+++ net/qrtr/qrtr.c
@@ -339,18 +339,12 @@ static int qrtr_node_enqueue(struct qrtr_node *node, struct sk_buff *skb,
 	size_t len = skb->len;
 	int rc, confirm_rx;
 
-<<<<<<< HEAD
 	confirm_rx = qrtr_tx_wait(node, to->sq_node, to->sq_port, type);
 	if (confirm_rx < 0) {
 		kfree_skb(skb);
 		return confirm_rx;
 	}
 
-<<<<<<< HEAD
-=======
->>>>>>> 194ccc88297a... net: qrtr: Support decoding incoming v2 packets
-=======
->>>>>>> 194ccc88297a... net: qrtr: Support decoding incoming v2 packets
 	hdr = skb_push(skb, sizeof(*hdr));
 	hdr->version = cpu_to_le32(QRTR_PROTO_VER_1);
 	hdr->type = cpu_to_le32(type);
@@ -435,8 +429,6 @@ int qrtr_endpoint_post(struct qrtr_endpoint *ep, const void *data, size_t len)
 	struct qrtr_node *node = ep->node;
 	const struct qrtr_hdr_v1 *v1;
 	const struct qrtr_hdr_v2 *v2;
-<<<<<<< HEAD
-<<<<<<< HEAD
 	struct qrtr_sock *ipc;
 	struct sk_buff *skb;
 	struct qrtr_cb *cb;
@@ -445,9 +437,6 @@ int qrtr_endpoint_post(struct qrtr_endpoint *ep, const void *data, size_t len)
 	size_t hdrlen;
 
 	if (len == 0 || len & 3)
-=======
-=======
->>>>>>> 194ccc88297a... net: qrtr: Support decoding incoming v2 packets
 	struct sk_buff *skb;
 	struct qrtr_cb *cb;
 	unsigned int size;
@@ -455,10 +444,6 @@ int qrtr_endpoint_post(struct qrtr_endpoint *ep, const void *data, size_t len)
 	size_t hdrlen;
 
 	if (len & 3)
-<<<<<<< HEAD
->>>>>>> 194ccc88297a... net: qrtr: Support decoding incoming v2 packets
-=======
->>>>>>> 194ccc88297a... net: qrtr: Support decoding incoming v2 packets
 		return -EINVAL;
 
 	skb = netdev_alloc_skb(NULL, len);
@@ -469,15 +454,11 @@ int qrtr_endpoint_post(struct qrtr_endpoint *ep, const void *data, size_t len)
 
 	/* Version field in v1 is little endian, so this works for both cases */
 	ver = *(u8*)data;
-<<<<<<< HEAD
 
 	switch (ver) {
 	case QRTR_PROTO_VER_1:
-<<<<<<< HEAD
 		if (len < sizeof(*v1))
 			goto err;
-=======
->>>>>>> 194ccc88297a... net: qrtr: Support decoding incoming v2 packets
 		v1 = data;
 		hdrlen = sizeof(*v1);
 
@@ -491,11 +472,8 @@ int qrtr_endpoint_post(struct qrtr_endpoint *ep, const void *data, size_t len)
 		size = le32_to_cpu(v1->size);
 		break;
 	case QRTR_PROTO_VER_2:
-<<<<<<< HEAD
 		if (len < sizeof(*v2))
 			goto err;
-=======
->>>>>>> 194ccc88297a... net: qrtr: Support decoding incoming v2 packets
 		v2 = data;
 		hdrlen = sizeof(*v2) + v2->optlen;
 
@@ -520,17 +498,13 @@ int qrtr_endpoint_post(struct qrtr_endpoint *ep, const void *data, size_t len)
 
 	if (len != ALIGN(size, 4) + hdrlen)
 		goto err;
-
-<<<<<<< HEAD
 	if (cb->dst_port != QRTR_PORT_CTRL && cb->type != QRTR_TYPE_DATA &&
 	    cb->type != QRTR_TYPE_RESUME_TX)
 		goto err;
-=======
 	if (cb->dst_port != QRTR_PORT_CTRL && cb->type != QRTR_TYPE_DATA)
 		goto err;
 
 	skb_put_data(skb, data + hdrlen, size);
->>>>>>> 194ccc88297a... net: qrtr: Support decoding incoming v2 packets
 
 	skb_put_data(skb, data + hdrlen, size);
 
@@ -573,6 +547,53 @@ int qrtr_endpoint_post(struct qrtr_endpoint *ep, const void *data, size_t len)
 		pr_err("qrtr: Invalid version %d\n", ver);
 		goto err;
 	}
+=======
+
+	switch (ver) {
+	case QRTR_PROTO_VER_1:
+		v1 = data;
+		hdrlen = sizeof(*v1);
+
+		cb->type = le32_to_cpu(v1->type);
+		cb->src_node = le32_to_cpu(v1->src_node_id);
+		cb->src_port = le32_to_cpu(v1->src_port_id);
+		cb->confirm_rx = !!v1->confirm_rx;
+		cb->dst_node = le32_to_cpu(v1->dst_node_id);
+		cb->dst_port = le32_to_cpu(v1->dst_port_id);
+
+		size = le32_to_cpu(v1->size);
+		break;
+	case QRTR_PROTO_VER_2:
+		v2 = data;
+		hdrlen = sizeof(*v2) + v2->optlen;
+
+		cb->type = v2->type;
+		cb->confirm_rx = !!(v2->flags & QRTR_FLAGS_CONFIRM_RX);
+		cb->src_node = le16_to_cpu(v2->src_node_id);
+		cb->src_port = le16_to_cpu(v2->src_port_id);
+		cb->dst_node = le16_to_cpu(v2->dst_node_id);
+		cb->dst_port = le16_to_cpu(v2->dst_port_id);
+
+		if (cb->src_port == (u16)QRTR_PORT_CTRL)
+			cb->src_port = QRTR_PORT_CTRL;
+		if (cb->dst_port == (u16)QRTR_PORT_CTRL)
+			cb->dst_port = QRTR_PORT_CTRL;
+
+		size = le32_to_cpu(v2->size);
+		break;
+	default:
+		pr_err("qrtr: Invalid version %d\n", ver);
+		goto err;
+	}
+
+	if (len != ALIGN(size, 4) + hdrlen)
+		goto err;
+
+	if (cb->dst_port != QRTR_PORT_CTRL && cb->type != QRTR_TYPE_DATA)
+		goto err;
+
+	skb_put_data(skb, data + hdrlen, size);
+>>>>>>> 194ccc88297a... net: qrtr: Support decoding incoming v2 packets
 
 	if (len != ALIGN(size, 4) + hdrlen)
 		goto err;
@@ -630,7 +651,11 @@ static struct sk_buff *qrtr_alloc_ctrl_packet(struct qrtr_ctrl_pkt **pkt,
 
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 	skb = alloc_skb(QRTR_HDR_MAX_SIZE + pkt_len, flags);
+=======
+	skb = alloc_skb(QRTR_HDR_MAX_SIZE + pkt_len, GFP_KERNEL);
+>>>>>>> 194ccc88297a... net: qrtr: Support decoding incoming v2 packets
 =======
 	skb = alloc_skb(QRTR_HDR_MAX_SIZE + pkt_len, GFP_KERNEL);
 >>>>>>> 194ccc88297a... net: qrtr: Support decoding incoming v2 packets
