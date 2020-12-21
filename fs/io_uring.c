952e177e7287675742b8563a7b3cbd6cce7222d1
Pavel Begunkov <asml.silence@gmail.com>
2020-12-21 18:34:05 +0000

    io_uring: fix double io_uring free
    Once we created a file for current context during setup, we should not
    call io_ring_ctx_wait_and_kill() directly as it'll be done by fput(file)
    
    Cc: stable@vger.kernel.org # 5.10
    Reported-by: syzbot+c9937dfb2303a5f18640@syzkaller.appspotmail.com
    Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>
    [axboe: fix unused 'ret' for !CONFIG_UNIX]
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- fs/io_uring.c
+++ fs/io_uring.c
@@ -11330,7 +11330,10 @@ static int io_uring_create(unsigned entries, struct io_uring_params *p,
 	struct io_ring_ctx *ctx;
 	struct file *file;
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
+=======
+>>>>>>> 9faadcc8abe4... io_uring: fix double io_uring free
 	bool limit_mem;
 >>>>>>> 9faadcc8abe4... io_uring: fix double io_uring free
 	int ret;
@@ -11442,7 +11445,10 @@ static int io_uring_create(unsigned entries, struct io_uring_params *p,
 	ret = io_uring_install_fd(ctx, file);
 	if (ret < 0) {
 <<<<<<< HEAD
+<<<<<<< HEAD
 		io_disable_sqo_submit(ctx);
+=======
+>>>>>>> 9faadcc8abe4... io_uring: fix double io_uring free
 =======
 >>>>>>> 9faadcc8abe4... io_uring: fix double io_uring free
 		/* fput will clean it up */
