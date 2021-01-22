363489bfc53724e1751f45ff5e3e1560ed84e5e1
Saravana Kannan <saravanak@google.com>
2021-01-22 11:35:59 -0800

    gpiolib: Bind gpio_device to a driver to enable fw_devlink=on by default
    There are multiple instances of GPIO device tree nodes of the form:
    
    foo {
    	compatible = "acme,foo";
    	...
    
    	gpio0: gpio0@xxxxxxxx {
    		compatible = "acme,bar";
    		...
    		gpio-controller;
    	};
    
    	gpio1: gpio1@xxxxxxxx {
    		compatible = "acme,bar";
    		...
    		gpio-controller;
    	};
    
    	...
    }
    
    bazz {
    	my-gpios = <&gpio0 ...>;
    }
    
    Case 1: The driver for "foo" populates struct device for these gpio*
    nodes and then probes them using a driver that binds with "acme,bar".
    This driver for "acme,bar" then registers the gpio* nodes with gpiolib.
    This lines up with how DT nodes with the "compatible" property are
    typically converted to struct devices and then registered with driver
    core to probe them. This also allows the gpio* devices to hook into all
    the driver core capabilities like runtime PM, probe deferral,
    suspend/resume ordering, device links, etc.
    
    Case 2: The driver for "foo" doesn't populate struct devices for these
    gpio* nodes before registering them with gpiolib. Instead it just loops
    through its child nodes and directly registers the gpio* nodes with
    gpiolib.
    
    Drivers that follow case 2 cause problems with fw_devlink=on. This is
    because fw_devlink will prevent bazz from probing until there's a struct
    device that has gpio0 as its fwnode (because bazz lists gpio0 as a GPIO
    supplier). Once the struct device is available, fw_devlink will create a
    device link with gpio0 device as the supplier and bazz device as the
    consumer. After this point, since the gpio0 device will never bind to a
    driver, the device link will prevent bazz device from ever probing.
    
    Finding and refactoring all the instances of drivers that follow case 2
    will cause a lot of code churn and it is not something that can be done
    in one shot. In some instances it might not even be possible to refactor
    them cleanly. Examples of such instances are [1] [2].
    
    This patch works around this problem and avoids all the code churn by
    simply setting the fwnode of the gpio_device and creating a stub driver
    to bind to the gpio_device. This allows all the consumers to continue
    probing when the driver follows case 2.
    
    [1] - https://lore.kernel.org/lkml/20201014191235.7f71fcb4@xhacker.debian/
    [2] - https://lore.kernel.org/lkml/e28e1f38d87c12a3c714a6573beba6e1@kernel.org/
    
    Fixes: e590474768f1 ("driver core: Set fw_devlink=on by default")
    Cc: Marc Zyngier <maz@kernel.org>
    Cc: Jisheng Zhang <Jisheng.Zhang@synaptics.com>
    Cc: Kever Yang <kever.yang@rock-chips.com>
    Reviewed-by: Linus Walleij <linus.walleij@linaro.org>
    Acked-by: Bartosz Golaszewski <bgolaszewski@baylibre.com>
    Signed-off-by: Saravana Kannan <saravanak@google.com>
    Link: https://lore.kernel.org/r/20210122193600.1415639-1-saravanak@google.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/gpio/gpiolib.c
+++ drivers/gpio/gpiolib.c
@@ -4211,6 +4211,7 @@ void gpiod_put_array(struct gpio_descs *descs)
 }
 EXPORT_SYMBOL_GPL(gpiod_put_array);
 
+<<<<<<< HEAD
 
 static int gpio_bus_match(struct device *dev, struct device_driver *drv)
 {
@@ -4223,6 +4224,8 @@ static int gpio_bus_match(struct device *dev, struct device_driver *drv)
 	return 1;
 }
 
+=======
+>>>>>>> 4731210c09f5... gpiolib: Bind gpio_device to a driver to enable fw_devlink=on by default
 static int gpio_stub_drv_probe(struct device *dev)
 {
 	/*
