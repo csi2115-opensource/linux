08de26eba5972368af1e4156a6abede17fa9fa39
Vitaly Kuznetsov <vkuznets@redhat.com>
2021-01-26 14:48:14 +0100

    KVM: x86: hyper-v: Make Hyper-V emulation enablement conditional
    Hyper-V emulation is enabled in KVM unconditionally. This is bad at least
    from security standpoint as it is an extra attack surface. Ideally, there
    should be a per-VM capability explicitly enabled by VMM but currently it
    is not the case and we can't mandate one without breaking backwards
    compatibility. We can, however, check guest visible CPUIDs and only enable
    Hyper-V emulation when "Hv#1" interface was exposed in
    HYPERV_CPUID_INTERFACE.
    
    Note, VMMs are free to act in any sequence they like, e.g. they can try
    to set MSRs first and CPUIDs later so we still need to allow the host
    to read/write Hyper-V specific MSRs unconditionally.
    
    Signed-off-by: Vitaly Kuznetsov <vkuznets@redhat.com>
    Message-Id: <20210126134816.1880136-14-vkuznets@redhat.com>
    [Add selftest vcpu_set_hv_cpuid API to avoid breaking xen_vmcall_test. - Paolo]
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/hyperv.c
+++ arch/x86/kvm/hyperv.c
@@ -1486,11 +1486,6 @@ int kvm_hv_set_msr_common(struct kvm_vcpu *vcpu, u32 msr, u64 data, bool host)
 	if (!host && !vcpu->arch.hyperv_enabled)
 		return 1;
 
-	if (!to_hv_vcpu(vcpu)) {
-		if (kvm_hv_vcpu_init(vcpu))
-			return 1;
-	}
-
 	if (kvm_hv_msr_partition_wide(msr)) {
 		int r;
 
@@ -1508,11 +1503,6 @@ int kvm_hv_get_msr_common(struct kvm_vcpu *vcpu, u32 msr, u64 *pdata, bool host)
 
 	if (!host && !vcpu->arch.hyperv_enabled)
 		return 1;
-
-	if (!to_hv_vcpu(vcpu)) {
-		if (kvm_hv_vcpu_init(vcpu))
-			return 1;
-	}
 
 	if (kvm_hv_msr_partition_wide(msr)) {
 		int r;
